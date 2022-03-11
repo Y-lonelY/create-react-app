c558529dd60bbb502d1d9cc1e8f86c30a4cde0ad
Y-lonelY <luckyyoung666@gmail.com>
2022-03-11 16:12:43 +0800

    chore: rename

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -367,6 +367,7 @@ module.exports = function (
 
   console.log();
   console.log(`Success! Created ${appName} at ${appPath}`);
+  console.log(chalk.red(` Current react-scripts version is ${appPackage.name}`))
   console.log('Inside that directory, you can run several commands:');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} start`));
